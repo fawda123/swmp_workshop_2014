0b6f0ecf0f555fdf3bbbc7acf2202cf2c9d2c06e
fawda123 <mbafs2012@gmail.com>
2014-10-20 11:12:40 -0500

    folders for data, scripts for each training module

--- training_modules/scripts/script1.R
+++ training_modules/scripts/script1.R
@@ -14,6 +14,9 @@ library(SWMPr)
 ## location of data, you will have to change the drive
 mypath <- 'C:/data/dataset1'
 
+## files are also stored in the package
+## mypath <- system.file('zip_ex', package = 'SWMPr')
+
 ## import and assign to 'dat'
 dat <- import_local(mypath, 'apacpwq', trace = T)
 
