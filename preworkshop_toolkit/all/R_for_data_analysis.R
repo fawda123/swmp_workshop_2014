9cf33f13271631b9a8bf19473d3f0a4cbbeb306a
fawda123 <mbafs2012@gmail.com>
2014-10-23 10:11:53 -0500

    complete draft of toolkit

---
+++ preworkshop_toolkit/all/R_for_data_analysis.R
@@ -0,0 +1,147 @@
+
+
+# create variable as a numeric vector
+a <- c(1, 2)
+
+# verify that its in our workspace
+ls()
+
+
+
+# data not loaded, only 'a' from before
+ls() 
+# load data as comma separated, assign to dat
+# make sure you are in the correct working directory
+# e.g., setwd('C:/my_directory') 
+dat <- read.table('dat_example.txt',sep = ',', header = T)
+ls() 
+
+
+
+head(dat)
+
+
+
+tail(dat)
+
+
+
+class(dat)
+
+
+
+dim(dat)
+nrow(dat)
+ncol(dat)
+
+
+
+summary(dat)
+
+
+
+names(dat)
+
+
+
+# these all do the same thing
+
+# get using $
+dat$do_mgl
+
+# get using [row, column] with variable name
+dat[, 'do_mgl']
+
+# get using [row, column] with column index
+dat[, 2]
+
+
+
+summary(dat$do_mgl)
+
+
+
+# note use of na.rm = T, you must specify how to handle missing values
+mean(dat$do_mgl, na.rm = T)
+range(dat$do_mgl, na.rm = T)
+var(dat$do_mgl, na.rm = T)
+
+
+
+hist(dat$do_mgl)
+
+
+
+boxplot(dat$do_mgl)
+
+
+
+# a nicer looking boxplot
+boxplot(dat$do_mgl, 
+  ylab = 'DO (mg/L)', 
+  main = 'Boxplot of dissolved oxygen',
+  col = 'lightblue'
+  )
+# see ?boxplot for all options
+
+
+
+# find the outliers using boxplot
+myplot <- boxplot(dat$do_mgl, plot = F)
+myplot$out
+
+
+
+# find the rows of the outliers
+outlier <- myplot$out
+out_row <- which(dat$do_mgl %in% outlier) 
+out_row #these are the row number of the outliers
+
+
+
+dat[out_row, ] # view the outliers
+
+
+
+## dat[out_row, 'do_mgl'] <- NA
+
+
+
+## dat[out_row, 'do_mgl'] <- mean(dat$do_mgl, na.rm = T)
+
+
+
+#first we have to convert the datetimestamp column
+dat$datetimestamp <- as.POSIXct(dat$datetimestamp)
+
+# plot the time series, y vs x syntax
+plot(do_mgl ~ datetimestamp, data = dat, type = 'l')
+
+
+
+# plot the time series
+plot(do_mgl ~ datetimestamp, data = dat, type = 'l')
+
+# use the out_row object from earlier to subset
+x <- dat[out_row, 'datetimestamp']
+y <- dat[out_row, 'do_mgl']
+points(x, y, col = 'red')
+
+
+
+# the original dataset
+dat_orig <- dat$do_mgl
+
+# dataset with outliers removed
+dat_remove <- dat$do_mgl
+dat_remove[out_row] <- NA
+
+# a boxplot comparison of the two datasets
+boxplot(dat_orig, dat_remove)
+
+
+
+# a t-test, evaluates the null that the difference in means is zero
+t.test(dat_orig, dat_remove)
+
+
---
+++ preworkshop_toolkit/all/intro_to_r.R
@@ -0,0 +1,177 @@
+# here's some R code
+rnorm(10)
+
+
+
+# download from CRAN
+install.packages('ggplot2')
+
+
+
+# load ggplot2
+library(ggplot2)
+
+
+
+# download devtools from CRAN
+install.package('devtools')
+
+# load devtools
+library(devtools)
+
+# install SWMPr from Github, load
+install_github('fawda123/SWMPr')
+library(SWMPr)
+
+
+
+# view packages with demos
+demo(package = .packages(all.available = TRUE))
+
+
+
+# view a demo for the graphics package
+demo(graphics)
+
+
+
+# view help files for a package
+help(package = 'ggplot2')
+
+
+
+# get help file
+help(mean, package = 'base')
+
+# or do this
+?mean
+
+
+
+print('hello world!')
+2 + 2
+(2 + 2) / 4
+
+
+
+seq(1, 30) # create a sequence of numbers from 1 to 30
+
+
+
+# getting an error
+2 + a
+
+
+
+# note the difference
+2 + 'a'
+
+
+
+a <- 1
+2 + a
+a = 1
+2 + a
+
+
+
+a <- c(1, 2, 3 , 4)
+a
+a <- seq(1, 4)
+a
+a <- c('a', 'b', 'c')
+a
+
+
+
+c(1, 2) # concatenate function to combine value
+mean(c(1, 2)) # mean function
+seq(1, 4) # create a sequence of values
+
+
+
+class(1)
+class('1')
+
+
+
+# this does not work
+'1' + 1
+
+
+
+a <- c(1,2) # numeric vector
+a
+b <- c('a', 'b') # character
+b
+
+
+
+# data frame with both
+c <- data.frame(a,b)
+c
+
+
+
+## # the location where you want to work
+## setwd('C:/my_directory')
+
+
+
+# use read.csv if .csv
+dat <- read.csv('my_data.csv', header = T)
+
+# use read.table if .txt
+dat <- read.table('my_data.txt', sep = ',', header = T)
+
+
+
+dat
+
+
+
+dat[1, ] # row 1
+dat[, 2] # column 2
+dat[4, 1] # row 4, column 1
+
+
+
+dat$Value
+dat[, 'Value']
+
+
+
+attach(dat)
+Value
+
+
+
+# use View to see the data
+View(dat)
+
+
+
+# view the objects in your workspace
+ls()
+
+
+
+## # save only the dat object
+## save(dat, file = 'my_data.RData')
+## 
+## # load .RData file of dat
+## load(file = 'my_data.RData')
+
+
+
+## # save as a csv file
+## write.csv(dat, 'my_data.csv')
+## 
+## # the defaults for write.csv can be changed
+## write.csv(dat, 'my_data.csv', quote = F, row.names = F)
+## 
+## # save as a txt file, comma separated
+## write.table(dat, 'my_data.txt', sep = ',', quote = F,
+##   row.names = F)
+
+
