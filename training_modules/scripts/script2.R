259125d9eca3312f0092c46327c15ca355ffda8a
fawda123 <mbafs2012@gmail.com>
2014-10-30 16:10:07 -0500

    finished data organize module, renamed

---
+++ training_modules/scripts/script2.R
@@ -0,0 +1,265 @@
+## script 2, NERRS training workshop 2014
+
+# reload the SWMPr package if you started a new session
+library(SWMPr)
+
+# import data
+# change this path for the flash drive
+path <- 'C:/data/dataset2'
+wq_dat <- import_local(path, 'apacpwq')
+nut_dat <- import_local(path, 'apacpnut')
+met_dat <- import_local(path, 'apaebmet')
+
+# what are the dimenions of the water quality data?
+dim(wq_dat)
+# what are the dimenions of the nutrient data?
+dim(nut_dat)
+# what are the dimenions of the weather data?
+dim(met_dat)
+
+# View the first six rows of the met data
+head(met_dat)
+
+# View the last six rows of the met data
+tail(met_dat)
+
+# class of the data
+class(met_dat)
+
+# what functions/methods work with swmpr objects?
+methods(class = 'swmpr')
+
+# see help for a swmpr function
+?aggregate.swmpr
+
+# or...
+help('aggregate.swmpr')
+
+# view the methods that apply to the generic aggregate
+methods('aggregate')
+
+# shorthand for executing aggregate on a swmpr object
+aggregate(met_dat, by = 'quarters')
+
+# long also works
+aggregate.swmpr(met_dat, by = 'quarters')
+
+# what attributes are available for a swmpr object
+names(attributes(met_dat))
+
+# view the parameters
+attr(met_dat, 'parameters')
+
+# view all attributes
+attributes(met_dat)
+
+# View the first six rows of the met data
+head(met_dat)
+
+# use qaqcchk to view distribution of qaqc flags
+myqaqc <- qaqcchk(met_dat)
+
+# a subset of results from the qaqcchk function
+head(myqaqc)
+
+# or view all in a separate window
+View(myqaqc)
+
+# select values that did not pass qaqc
+nopass <- grep('0', met_dat$f_totpar, invert = T)
+nopass <- met_dat[nopass, ]
+
+# plot totpar from met_dat
+plot(totpar ~ datetimestamp, met_dat, type = 'l')
+
+# add  points that did not pass qaqc
+points(nopass$datetimestamp, nopass$totpar, col = 'red')
+
+# filter observations by qaqc flags, remove qaqc columns
+met_qaqc <- qaqc(met_dat)
+
+# view help file
+?qaqc
+
+# data after qaqc processing
+head(met_qaqc)
+
+# keep all values
+met_qaqc <- qaqc(met_dat, qaqc_keep = NULL)
+
+head(met_qaqc) # note the totpar column compared to the last example
+
+# keep all values
+to_keep <- c(-5, -4, -3, -2, -1, 1, 2, 3, 4, 5)
+met_qaqc <- qaqc(met_dat, qaqc_keep = to_keep)
+
+# does this result make sense??
+head(met_qaqc)
+
+# continue with qaqc processed data
+
+# water quality
+# note the column number before/after qaqc processing
+dim(wq_dat)
+wq_dat <- qaqc(wq_dat)
+dim(wq_dat)
+
+# nutrients
+nut_dat <- qaqc(nut_dat)
+
+# weather
+met_dat <- qaqc(met_dat)
+
+# view help file
+?subset.swmpr
+
+# view the arguments for subset.swmpr
+formals(subset.swmpr)
+
+# rows, columns in wq_dat
+dim(wq_dat)
+
+# remove empty rows, columns
+tmp <- subset(wq_dat, rem_rows = T, rem_cols = T)
+
+# dimensions after removing empty rows, columns
+dim(tmp)
+
+# select the DO column
+tmp <- subset(wq_dat, select = 'do_mgl')
+head(tmp)
+
+# select DO and salinity
+tmp <- subset(wq_dat, select = c('do_mgl', 'sal'))
+head(tmp)
+
+# incorrect syntax
+subset(wq_dat, select = 'do_mgl', 'sal')
+
+# select a date range, July 2012
+dates <- c('2012-07-01 12:00', '2012-07-31 6:30')
+tmp <- subset(wq_dat, subset = dates)
+head(tmp) # view first six rows
+
+# check the date_rng attribute
+class(tmp) # a swmpr object?
+
+# what are the attributes?
+names(attributes(tmp))
+
+# get the date range
+attr(tmp, 'date_rng')
+
+# get observations for 2013
+dates <- '2013-01-01 00:00'
+tmp <- subset(wq_dat, subset = dates, operator = '>=')
+head(tmp)
+
+# dates and parameters  to select
+dates <- '2014-10-01 00:00'
+params <- c('do_mgl', 'temp')
+
+# subset
+tmp <- subset(wq_dat, select = params, subset = dates, operator = '>=')
+head(tmp)
+
+# plot DO and water temp
+plot(do_mgl ~ datetimestamp, data = tmp, type = 'l')
+plot(temp ~ datetimestamp, data = tmp, type = 'l')
+
+# incorrect subset argument format, no time
+subset(wq_dat, subset = c('2012-01-01', '2012-01-31'))
+
+# forgot to include operator
+subset(wq_dat, subset = '2012-01-31 00:00')
+
+# incorrect parameter names
+subset(wq_dat, select = 'DO')
+
+# help files
+?comb.swmpr
+?setstep.swmpr
+
+# combine water quality and weather data in the same object
+tmp <- comb(wq_dat, met_dat)
+head(tmp, 3) # first three rows
+
+# attributes of combined object
+
+# stations 
+attr(tmp, 'station')
+
+# date ranges, same as before
+attr(tmp, 'date_rng')
+
+# parameters, both wq and met
+attr(tmp, 'parameters')
+
+# plot some combined data
+
+# subset date ranges first
+dates <- c('2012-06-01 0:0', '2012-07-31 0:0')
+to_plot <- subset(tmp, subset = dates)
+
+# plot
+plot(atemp ~ datetimestamp, to_plot, type = 'l', col = 'red', ylab = 'Temp C')
+lines(to_plot$datetimestamp, to_plot$temp, col = 'blue')
+title('Air (red) and water (blue) temperature')
+
+# arguments for the function, or ?comb.swmpr
+formals(comb.swmpr)
+
+# dimension of earlier combined object
+dim(tmp)
+
+# create new object at two hour time step
+tmp <- comb(wq_dat, met_dat, timestep = 120)
+dim(tmp)
+
+# note the time step in datetimestamp
+head(tmp, 4)
+
+# note the pre-existing time steps
+head(wq_dat$datetimestamp)
+
+# note irregular time stamps on nutrient data
+head(nut_dat)
+
+dim(nut_dat) # initial dimensions
+
+# standardize nutrient time series
+tmp <- setstep(nut_dat, timestep = 60, differ = 5)
+dim(tmp)
+
+# remove empty rows, columns
+tmp <- subset(tmp, rem_row = T, rem_col = T)
+tmp #only four rows!
+
+# first we subset for the example
+sub1 <- subset(wq_dat, subset = c('2012-07-01 0:0', '2012-07-31 0:0'))
+sub2 <- subset(met_dat, subset = c('2012-07-15 0:0', '2012-08-12 0:0'))
+
+# what are the date ranges?
+attr(sub1, 'date_rng')
+attr(sub2, 'date_rng')
+
+# combine using union, this is the default
+tmp <- comb(sub1, sub2, method = 'union')
+
+# what is the date range of the combined object?
+attr(tmp, 'date_rng')
+
+# combine using intersect
+tmp <- comb(sub1, sub2, method = 'intersect')
+
+# what is the date range of the combined object?
+attr(tmp, 'date_rng')
+
+# combine using the time range in sub1
+station <- attr(sub1, 'station')
+tmp <- comb(sub1, sub2, method = station)
+
+# what is the date range of the combined object?
+attr(tmp, 'date_rng')
+
+
